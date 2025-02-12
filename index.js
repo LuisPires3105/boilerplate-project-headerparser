8fac7ba84fc8fd10b88fb7179b863d4d35b74478
Luis Pires <luispires310500@gmail.com>
2025-02-12 13:45:00 +0000

    Resolving issue with spelling in index.js file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.set('trust proxy', true);
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -24,12 +25,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// your second API endpoint...
-app.get('/api/whoami', function (req, res) {
-  res.json({ ipadress: `${req.ip}`, language: `${req.headers['accept-language']}`, software: `${req.headers['user-agent']}` });
-});
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// your second API endpoint...
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress: `${req.ip}`, language: `${req.headers['accept-language']}`, software: `${req.headers['user-agent']}` });
 });
